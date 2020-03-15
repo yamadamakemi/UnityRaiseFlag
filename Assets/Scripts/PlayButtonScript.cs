84075c6299e692297011de637706951cd49559c1
yamadamakemi <yamadamakemi@gmail.com>
2020-03-15 20:54:35 +0900

    ボタンを押したらゲーム画面に遷移

---
+++ Assets/Scripts/PlayButtonScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // ボタンが押された時の処理
+    public void OnClick()
+    {
+        //Debug.Log("Text: test");
+        // シーンの切り替え
+        SceneManager.LoadScene("GameScene");
+    }
+}

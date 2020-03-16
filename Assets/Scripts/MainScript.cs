e550c5f1f1a9326900e3759525de9b6eda14c6fc
yamadamakemi <yamadamakemi@gmail.com>
2020-03-16 23:00:54 +0900

    [ゲーム画面機能]MainScript作成

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Text: MainScript");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

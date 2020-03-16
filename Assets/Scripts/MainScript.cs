82ec331fd6b14e768465d48cad895b5fabba97cd
yamadamakemi <yamadamakemi@gmail.com>
2020-03-16 23:20:19 +0900

    [ゲーム画面機能]カウントダウンタイマー作成

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainScript : MonoBehaviour
 {
+    private float limitTime;
+    public Text textTimer;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Text: MainScript");
+        // 残り時間の初期値
+        limitTime = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 残り時間をtextTimerに表示
+        float v = Mathf.Round(limitTime - Time.time);
+        textTimer.text = "残り時間 : " + v + "秒";
     }
 }

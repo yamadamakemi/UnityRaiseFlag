3b4f2d5a1012f845ed8d3fa6252b14484a135cfa
yamadamakemi <yamadamakemi@gmail.com>
2020-03-18 08:22:39 +0900

    [ゲーム画面機能]左右のボタンを押した時の処理を追加

---
+++ Assets/Scripts/LeftButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // ボタンが押された時の処理
+    public void OnClick()
+    {
+        Debug.Log("Text: LeftClick");
+    }
+}

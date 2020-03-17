c6c9bff85aeecc7f240a323ad766dbfb14be09fe
yamadamakemi <yamadamakemi@gmail.com>
2020-03-18 01:07:32 +0900

    [ゲーム画面機能]左右どちらか(赤か白)をランダムで表示

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -7,12 +7,19 @@ public class MainScript : MonoBehaviour
 {
     private float limitTime;
     public Text textTimer;
+    public Text textFlag;
 
     // Start is called before the first frame update
     void Start()
     {
         // 残り時間の初期値
         limitTime = 10;
+
+        // 赤か白をランダムに表示
+        // TODO 赤と白を配列に入れて、ランダムで出す
+        int v = Random.Range(0, 5);
+        char v1 = (v < 3) ? '白' : '赤';
+        textFlag.text = v1.ToString();
     }
 
     // Update is called once per frame

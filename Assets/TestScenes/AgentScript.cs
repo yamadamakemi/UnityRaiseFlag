a8015c04afa6ef15d8061e5755c33edd99d0f7b6
yamadamakemi <yamadamakemi@gmail.com>
2020-03-29 15:58:23 +0900

    [アニメーション作成]キャラクターの動き（手をあげる）

---
+++ Assets/TestScenes/AgentScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentScript : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            Debug.Log("right");
+            anim.SetTrigger("isRight");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("left");
+            anim.SetTrigger("isLeft");
+        }
+    }
+}
---
+++ Assets/TestScenes/TestAnimationScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimationScript : MonoBehaviour
+{
+    public Animator Ani;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            print("up arrow key is held down");
+            Ani.SetBool("isHand", true);
+
+            Invoke("Reset", 0.3f);
+        }
+    }
+
+    private void Reset()
+    {
+        print("reset");
+        Ani.SetBool("isHand", false);
+    }
+}
